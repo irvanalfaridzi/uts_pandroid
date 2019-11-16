fd620ba5d9523a04d83ef0ad37c413fda9ae937e
irvanalfaridzi <irvanalfaridzi@gmail.com>
2019-11-16 22:17:16 +0700

    second commit

--- app/src/main/java/com/example/uts_pandroid/MainActivity.java
+++ app/src/main/java/com/example/uts_pandroid/MainActivity.java
@@ -156,15 +156,53 @@ public class MainActivity extends AppCompatActivity {
                 db.close();
                 Ngisine xx = new Ngisine(getApplication(),kode11, nama11, satuan11, hbeli11, hjual11, diskon11);
                 isilist.setAdapter(xx);
-                update.setEnabled(true);
-                delete.setEnabled(true);
+                update.setEnabled(false);
+                delete.setEnabled(false);
             }
         });
 
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                db=openOrCreateDatabase(dbname,MODE_PRIVATE,null);
 
+                kd = kode.getText().toString();
+                nm = nama.getText().toString();
+                st = satuan.getText().toString();
+                hb = hbeli.getText().toString();
+                hj = hjual.getText().toString();
+                dsk = diskon.getText().toString();
+
+                db.execSQL("delete from barang where kode = '"+ kd +"';");
+                Cursor c = db.rawQuery("select * from barang", null);
+                String kode11[] = new String[c.getCount()];
+                String nama11[] = new String[c.getCount()];
+                String satuan11[] = new String[c.getCount()];
+                String hbeli11[] = new String[c.getCount()];
+                String hjual11[] = new String[c.getCount()];
+                String diskon11[] = new String[c.getCount()];
+                x=1;
+                c.moveToFirst();
+                kode11[0] = c.getString(c.getColumnIndex("kode"));
+                nama11[0] = c.getString(c.getColumnIndex("nama"));
+                satuan11[0] = c.getString(c.getColumnIndex("satuan"));
+                hbeli11[0] = c.getString(c.getColumnIndex("hbeli"));
+                hjual11[0] = c.getString(c.getColumnIndex("hjual"));
+                diskon11[0] = c.getString(c.getColumnIndex("diskon"));
+                while(c.moveToNext()){
+                    kode11[x] = c.getString(c.getColumnIndex("kode"));
+                    nama11[x] = c.getString(c.getColumnIndex("nama"));
+                    satuan11[x] = c.getString(c.getColumnIndex("satuan"));
+                    hbeli11[x] = c.getString(c.getColumnIndex("hbeli"));
+                    hjual11[x] = c.getString(c.getColumnIndex("hjual"));
+                    diskon11[x] = c.getString(c.getColumnIndex("diskon"));
+                    x = x+1;
+                }
+                db.close();
+                Ngisine xx = new Ngisine(getApplication(),kode11, nama11, satuan11, hbeli11, hjual11, diskon11);
+                isilist.setAdapter(xx);
+                update.setEnabled(false);
+                delete.setEnabled(false);
             }
         });
 
@@ -185,21 +223,21 @@ public class MainActivity extends AppCompatActivity {
         isilist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                db=openOrCreateDatabase(dbname,MODE_PRIVATE,null);
-                HashMap<String, Object> obj = (HashMap<String, Object>)adapterView.getItemAtPosition(i);
-                String kodeget = (String) obj.get("kode");
-                kode.setText(kodeget);
-                Cursor c = db.rawQuery("select * from barang where kode = '"+ kd +"';", null);
-                nm = c.getString(c.getColumnIndex("nama"));
-                st = c.getString(c.getColumnIndex("satuan"));
-                hb = c.getString(c.getColumnIndex("hbeli"));
-                hj = c.getString(c.getColumnIndex("hjual"));
-                dsk = c.getString(c.getColumnIndex("diskon"));
-                kode.setText(kd);
-                nama.setText(st);
-                hbeli.setText(hb);
-                hjual.setText(hj);
-                diskon.setText(dsk);
+//                db=openOrCreateDatabase(dbname,MODE_PRIVATE,null);
+//                HashMap<String, Object> obj = (HashMap<String, Object>)adapterView.getItemAtPosition(i);
+//                String kodeget = (String) obj.get("kode");
+//                kode.setText(kodeget);
+//                Cursor c = db.rawQuery("select * from barang where kode = '"+ kd +"';", null);
+//                nm = c.getString(c.getColumnIndex("nama"));
+//                st = c.getString(c.getColumnIndex("satuan"));
+//                hb = c.getString(c.getColumnIndex("hbeli"));
+//                hj = c.getString(c.getColumnIndex("hjual"));
+//                dsk = c.getString(c.getColumnIndex("diskon"));
+//                kode.setText(kd);
+//                nama.setText(st);
+//                hbeli.setText(hb);
+//                hjual.setText(hj);
+//                diskon.setText(dsk);
                 update.setEnabled(true);
                 delete.setEnabled(true);
             }
